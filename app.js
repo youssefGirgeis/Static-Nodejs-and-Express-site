8c10cdf014a9f6a3e0d685f686d98752f1b564bc
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-12-14 20:26:13 -0500

    create /about route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
 app.listen('3000', () => {
   console.log('app running on localhost 3000');
 });
