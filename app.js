92a34f6a5f5c95a41e832380dac2d941dadd5f88
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-12-15 14:34:29 -0500

    create /about route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const { projects } = data;
 
 app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index');
